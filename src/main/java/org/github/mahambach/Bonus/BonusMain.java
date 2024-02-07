6726d7a3ae2285df11915223533d37f71952de5a
mahambach <ma.hambach@gmx.net>
2024-02-07 16:41:53 +0100

    add Bonus done

--- src/main/java/org/github/mahambach/Bonus/BonusMain.java
+++ src/main/java/org/github/mahambach/Bonus/BonusMain.java
@@ -3,16 +3,25 @@ package org.github.mahambach.Bonus;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BonusMain {
     public static void main(String[] args) {
         try {
-            List<String> line = Files.lines(Path.of("students.csv"))
-                    .peek(System.out::println)
-                    .toList();
-            System.out.println(line);
+            Files.lines(Path.of("students.csv"))
+                    .skip(1)
+                    .filter(n-> !n.isEmpty())
+                    .map(n-> n.split(","))
+                    .map(n -> new Student(
+                            Integer.valueOf(n[0]),
+                            n[1],
+                            Integer.valueOf(n[2]),
+                            Integer.valueOf(n[3]))
+                    )
+                    .collect(Collectors.toSet())
+                    .forEach(System.out::println);
+
         }
         catch(IOException e){
             System.out.println("lol");

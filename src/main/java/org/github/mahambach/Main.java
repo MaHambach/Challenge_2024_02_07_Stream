0138f883ff22f9496670dd9f6a1506f3de6c7094
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-02-07 15:14:05 +0100

    Tasks added as comment

--- src/main/java/org/github/mahambach/Main.java
+++ src/main/java/org/github/mahambach/Main.java
@@ -1,5 +1,21 @@
 package org.github.mahambach;
 
+/*
+Step 1: Filter a list of numbers and only output the even numbers.
+Step 2: Use 'map' and double each number in the list.
+Step 3: Sort the list in ascending order.
+Step 4: Perform a 'reduce' operation to calculate the sum of all numbers in the list.
+Step 5: Use 'forEach' and output each processed number.
+Step 6: Collect the processed numbers into a new list using 'collect'.
+
+BONUS:
+Take a look at the file students.csv. Copy the file to the root directory of your project.
+Use streams to read and output the file line by line (Hint: Files.lines(Path.of("students.csv")))
+Remove the header
+Convert each line into the class Student
+Remove invalid lines and duplicates
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");

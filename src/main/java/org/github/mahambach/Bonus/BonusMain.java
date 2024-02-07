6f7646195b58aab4c75c506e630229c2741c7d98
mahambach <ma.hambach@gmx.net>
2024-02-07 17:15:22 +0100

    add Bonus done

--- src/main/java/org/github/mahambach/Bonus/BonusMain.java
+++ src/main/java/org/github/mahambach/Bonus/BonusMain.java
@@ -10,16 +10,35 @@ public class BonusMain {
     public static void main(String[] args) {
         try {
             Files.lines(Path.of("students.csv"))
-                    .skip(1)
-                    .filter(n-> !n.isEmpty())
-                    .map(n-> n.split(","))
-                    .map(n -> new Student(
+                    .skip(1)                                 //Überspringt die erste Zeile.
+                    .filter(n-> !n.isEmpty())                   //Löscht leere Strings.
+                    .map(n-> n.split(","))                //Teilt den String an allen "," auf in ein String-Array.
+                    .map(n -> new Student(                      //Für jedes der String-Arrays wird ein Student erstellt.
                             Integer.valueOf(n[0]),
                             n[1],
                             Integer.valueOf(n[2]),
                             Integer.valueOf(n[3]))
                     )
-                    .collect(Collectors.toSet())
+                    .collect(Collectors.toSet())                //Erstellt aus den Studenten eine Studenten-Menge. Dadurch werden Duplikate gelöscht.
+                    .stream()                                   //Da wir eine Menge haben, müssen wir hier einen neuen Stream starten.
+                    .collect(Collectors.toList())               //
+                    .stream()
+                    .sorted((a,b) -> a.getId() - b.getId())
+                    .forEach(System.out::println);
+
+            System.out.println();
+
+            Files.lines(Path.of("students.csv"))
+                    .skip(1)                                 //Überspringt die erste Zeile.
+                    .filter(n-> !n.isEmpty())                   //Löscht leere Strings.
+                    .map(n-> n.split(","))                //Teilt den String an allen "," auf in ein String-Array.
+                    .map(n -> new Student(                      //Für jedes der String-Arrays wird ein Student erstellt.
+                            Integer.valueOf(n[0]),
+                            n[1],
+                            Integer.valueOf(n[2]),
+                            Integer.valueOf(n[3]))
+                    )
+                    .distinct()                                  //Löscht Duplikate.
                     .forEach(System.out::println);
 
         }
